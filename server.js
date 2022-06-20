eabb2928c24c223888df5c528210fa2496cc304b
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-21 00:49:40 +0300

    set up server side API code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Setting up our project dependencies
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -23,3 +24,49 @@ MongoClient.connect(dbConnectionStr)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+// Setting up our routing
+
+// Search autocomplete functionality
+app.get('/search', async (request, response) => {
+    try {
+        let result = await collection.aggregate([
+            {
+                "$search" : {
+                    "autocomplete" : {
+                        // Passing in a specific query (made by our user)
+                        "query": `${request.query.query}`,
+                        // It will look for our titles
+                        "path": "title",
+                        "fuzzy": {
+                            // Allows the user to make two character errors without removing that query
+                            "maxEdits":2,
+                            // Autocomplete starts showing relevant results after three characters
+                            "prefixLength": 3
+                        }
+                    }
+                }
+            }
+        ]).toArray()
+        console.log(result)
+        response.send(result)
+    } catch(error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+// Routing for when the user has chosen their desired movie (from the dropdown list)
+app.get('/get/:id', async (request, response) => {
+    try {
+        let result = await collection.findOne({
+            "_id": ObjectId(request.params.id)
+        })
+        response.send(result)
+    } catch(error) {
+        response.status(500).send({ message: error.message })
+    }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
