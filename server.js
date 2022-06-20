517d2ba1f3491365447a9a084e907210610a562a
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-20 16:38:57 +0300

    add middleware and connect to mongo

--- server.js
+++ server.js
@@ -9,3 +9,17 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'sample_mflix',
     collection
+
+// Setting up our middleware
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+
+// Setting up our connection to the database
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to database`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
